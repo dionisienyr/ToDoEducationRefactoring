00a15947eb250276312779315f5326ddb9c13904
dionisie <dionisiegame@gmail.com>
2022-09-06 14:50:22 +0300

    js refactoring commit

--- js/home.js
+++ js/home.js
@@ -1,30 +1,26 @@
 const loadMore = document.querySelector('.load-more__btn');
-const taskLength = document.querySelectorAll('.wrapper__item').length;
+const taskCount = document.querySelectorAll('.wrapper__item').length;
 const deleteElements = document.querySelector('.delete__btn');
 const modalOverlay = document.querySelector('.modal-overlay')
-const modalWindow = document.querySelector('.modal-window');
-//const taskCheckbox = document.querySelectorAll(".menu-task-container__main input[type='checkbox']:checked");
 
 const acceptDeleteBtn = document.querySelector('.delete-accept__btn')
 const cancelDeleteBtn = document.querySelector('.delete-cancel__btn')
 let items = 8;
-    
 
-    console.log(taskLength)
 
-const printQuantity = () => {
-    let taskQuantity = document.querySelector('.info-quantity');
-    taskQuantity.innerHTML = taskLength;
+const printCountValue = () => {
+    let taskCountValue = document.querySelector('.info-quantity');
+    taskCountValue.innerHTML = taskCount;
 }
 
 
 
 loadMore.addEventListener('click', () => {
     items += 5 
-    const arrayElem = Array.from(document.querySelector('.wrapper-list').children);
-    const visItems = arrayElem.slice(0, items);
+    const arrayElements = Array.from(document.querySelector('.wrapper-list').children);
+    const showItems = arrayElements.slice(0, items);
 
-    visItems.forEach(el => el.classList.add('is-visible'));
+    showItems.forEach(el => el.classList.add('is-visible'));
 });
 
 deleteElements.addEventListener('click', (el) => {
@@ -48,12 +44,7 @@ modalOverlay.addEventListener('click', (e) => {
     }
 });
 
-
-//function remove(elem){
-//    let items = document.getElementsByClassName(elem).querySelectorAll(".menu-task-container__main input[type='checkbox']:checked");
-//    items.forEach(item => item.closest('.menu-task-container__main').remove());
-//}
-printQuantity()
+printCountValue()
 acceptDeleteBtn.addEventListener("click",() => {
 
     let checkbox = document.querySelectorAll(".menu-task-container__main input[type='checkbox']:checked")
@@ -61,18 +52,12 @@ acceptDeleteBtn.addEventListener("click",() => {
         item.closest('.menu-task').remove()
         
     });
-    printQuantity()
-    
-    
-    
-    
-    //checkbox.closest('.menu-task').remove();
-//let list = document.querySelectorAll(".menu-task");
-    //console.log(list);
-    //items += 1
-    //const arrayElem = Array.from(document.querySelector('.wrapper-list').children);
-    //const visItems = arrayElem.slice(0, items);
+    printCountValue()
 
-    //visItems.forEach(el => el.classList.add('is-visible'));
+    
 });
 
+loadMore.removeEventListener('click', addEventListener, false)
+deleteElements.removeEventListener('click', addEventListener,false);
+modalOverlay.removeEventListener('click', addEventListener, false)
+acceptDeleteBtn.removeEventListener('click', addEventListener, false)
--- js/script.js
+++ js/script.js
@@ -1,31 +1,13 @@
-const addTaskBtn = document.querySelector('.confirm-enabled__btn');
-const titleAdd = document.getElementById('title__value');
-const descrAdd = document.getElementById('description__value');
-const dueAdd = document.getElementById('due__value'); 
 
-const todosWrapper = document.querySelector('.wrapper-list')
+const addTaskBtn = document.querySelector('.confirm-enabled__btn');
+const title = document.getElementById('title__value');
+const description = document.getElementById('description__value');
+const due = document.getElementById('due__value'); 
+const toDoWrapperNodes = document.querySelector('.wrapper-list')
 
 
 let tasks;
 !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
-
-function Task(title, description, date){
-    this.title = title;
-    this.description = description;
-    this.date = date;
-    this.completed = false;
-}
-
-
-
-//const fillHtmlList = () => {
-//    todosWrapper.innerHTML = "";
-//    if(tasks.length > 0) {
-//        tasks.forEach((item, index) => {
-//            todosWrapper.innerHTML += createTemplate(item, index)
-//        })
-//    }
-//}
 
 const updateLocalStorage = () => {
     localStorage.setItem('tasks', JSON.stringify(tasks));
@@ -33,40 +15,23 @@ const updateLocalStorage = () => {
 
 if (addTaskBtn) {
 addTaskBtn.addEventListener('click', () => {
-    tasks.push(new Task(titleAdd.value, descrAdd.value, dueAdd.value));
+    tasks.push({title: title.value, description: description.value, due: due.value});
     updateLocalStorage();
 })
 }
 
-
-
-//const createTemplate = (test) => {
-//    return `
-//        <li class="wrapper__item">
-//            <div class="menu-task">
-//                <div class="menu-task-container__main">
-//                    <input type="checkbox" name="complete" class="menu-task__checkbox">
-//                    <p class="menu-task__due">${test[0].date}</p>
-//                </div>
-//                <div class="menu-task-container__side">
-//                    <p class="menu-task__text">${test[0].title}</p>
-//                </div>
-//            </div>
-//        </li>
-//    `
-//}
-
 let toDoList = JSON.parse(localStorage.getItem('tasks'))
-//console.log(toDoList)
-if(todosWrapper){
+if(toDoWrapperNodes){
+
     for (let toDo in toDoList){
-    let li = todosWrapper.appendChild(document.createElement('li'));
+    let li = toDoWrapperNodes.appendChild(document.createElement('li'));
     let menuTask = li.appendChild(document.createElement('div'));
     let taskData = menuTask.appendChild(document.createElement('div'));
     let taskCheckbox = taskData.appendChild(document.createElement('input'));
     let taskP = taskData.appendChild(document.createElement('p'));
     let taskTitle = menuTask.appendChild(document.createElement('div'));
     let titleItem = taskTitle.appendChild(document.createElement('p'));
+
     li.className = "wrapper__item";
     menuTask.className = "menu-task";
     taskData.className = "menu-task-container__main";
